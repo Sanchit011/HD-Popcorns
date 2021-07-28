1eec0bc32d3bb5cd26e46d8fa1ea99e221074d7d
sanchit <sanchit4242@gmail.com>
2021-07-28 09:29:58 +0530

    hover effect

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./listItem.scss";
 import {
   Add,
@@ -6,30 +7,44 @@ import {
   ThumbDownAltOutlined,
   ThumbUpAltOutlined,
 } from "@material-ui/icons";
-const ListItem = () => {
+const ListItem = ({ index }) => {
+  const [isHovered, setisHovered] = useState(false);
+  const trailer =
+    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
   return (
-    <div className="listItem">
+    <div
+      className="listItem"
+      style={{ left: isHovered && index * 227.5 - 50 }}
+      onMouseEnter={() => setisHovered(true)}
+      onMouseLeave={() => setisHovered(false)}
+    >
       <img
         src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
         alt=""
       />
-      <div className="itemInfo">
-        <div className="icons">
-          <PlayCircleFilledWhite />
-          <Add />
-          <ThumbUpAltOutlined />
-          <ThumbDownAltOutlined />
-        </div>
-        <div className="itemInfoTop">
-          <span>1 hour 40 min</span>
-          <span className="limit">+16</span>
-          <span>1999</span>
-        </div>
-        <div className="desc">
-          akdsnadn sdfafda sfda ssdfa sd asfd asfd asd ad asd as da d ad asd sad
-        </div>
-        <div className="genre">Action</div>
-      </div>
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayCircleFilledWhite className="icon" />
+              <Add className="icon" />
+              <ThumbUpAltOutlined className="icon" />
+              <ThumbDownAltOutlined className="icon" />
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 40 min</span>
+              <span className="limit">+16</span>
+              <span>1999</span>
+            </div>
+            <div className="desc">
+              akdsnadn sdfafda sfda ssdfa sd asfd asfd asd ad asd as da d ad asd
+              sad
+            </div>
+            <div className="genre">Action</div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
