3323ee356e2e696581f9e9d267dc5e9eaee3439a
sanchit <sanchit4242@gmail.com>
2021-07-28 11:52:57 +0530

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import "./app.scss";
 import Home from "./pages/home/Home";
 import Watch from "./pages/watch/Watch";
 import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 const App = () => {
-  return <Register />;
+  return <Login />;
 };
 
 export default App;
