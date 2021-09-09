9cf5c4abd7d588492d5a1e91d27370fa2dbe1158
Sanchit011 <sanchit4242@gmail.com>
2021-09-09 09:15:06 +0530

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./app.scss";
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/movies">
+          <Home type="movies" />
+        </Route>
+        <Route path="/series">
+          <Home type="series" />
+        </Route>
+        <Route path="/watch">
+          <Watch />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
